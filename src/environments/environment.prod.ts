4c570d1abcb2314a58be5222f500a0d9f7c72bf1
hoanghai03 <hoanghai03021999@gmail.com>
2026-05-17 21:08:38 +0700

    Cap nhat code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: '/api',
   proxyUrl: '',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
+  googleClientId: '1031323147350-iaespqed3hdrb175o5gsrqpp13n59kg7.apps.googleusercontent.com',
   facebookAppId: 'YOUR_FACEBOOK_APP_ID',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:5000/api',
   proxyUrl: 'http://localhost:3001',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
+  googleClientId: '1031323147350-iaespqed3hdrb175o5gsrqpp13n59kg7.apps.googleusercontent.com',
   facebookAppId: 'YOUR_FACEBOOK_APP_ID',
 };
